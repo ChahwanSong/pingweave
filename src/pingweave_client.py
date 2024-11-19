a7ec942ef37c5ff90c708936838649259afe918e
ChahwanSong <skychahwan@gmail.com>
2024-11-19 16:16:13 +0800

    UPDATE: remove dependency on asyncio.run which is not available under python <= 3.6

--- src/pingweave_client.py
+++ src/pingweave_client.py
@@ -146,4 +146,8 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(main())
+    finally:
+        loop.close()
